1ba5b86059d413cc1d675d7af2967f87ddbd42ee
duonglqdev <duonglq.dev@gmail.com>
2024-07-15 11:58:23 +0700

    update theme

--- theme/functions.php
+++ theme/functions.php
@@ -183,6 +183,8 @@ function gnws_scripts() {
 	wp_enqueue_script( 'gnws-js-fancybox', get_template_directory_uri() . '/assets/lib/jquery.fancybox.js', array(), GNWS_VERSION, true );
 	wp_enqueue_script( 'gnws-script', get_template_directory_uri() . '/js/script.min.js', array(), GNWS_VERSION, true );
 
+	wp_localize_script( 'gnws-script', 'ajaxurl',array('ajaxurl'=>admin_url( 'admin-ajax.php')));
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
